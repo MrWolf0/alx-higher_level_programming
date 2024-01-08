47947c05c83a3044d69a4514689c17942f26e881
MrWolf0 <nassermessi33@gmail.com>
2024-01-09 00:13:15 +0200

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    # checking my_list if it is a list object using isinstance.
+    # if true it will reverse the list using builtin reverse fun.
+    if isinstance(my_list, list):
+        my_list.reverse()
+    # printing the reversed list using formate function.
+    for index in my_list:
+        print("{:d}".format(index))
