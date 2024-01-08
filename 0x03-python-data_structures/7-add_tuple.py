80e91631920351716d685d185d3e74bde7426b44
MrWolf0 <nassermessi33@gmail.com>
2024-01-09 01:38:13 +0200

    task 3

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    # use the first two elements of each tuple or 0 if not present
+    # we take the first two elements of each tuple using
+    # slicing notation [:2].
+    # if the tuple has fewer than two elements
+    # this will take all available elements
+    # then we concatenate (0, 0) to ensure that tuple
+    # always has at least two elements.
+    # if tuple has more than two elements
+    # only the first two will be considered.
     a = tuple_a[:2] + (0, 0)
     b = tuple_b[:2] + (0, 0)
 
