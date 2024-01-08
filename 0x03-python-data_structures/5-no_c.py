05bbdd7033e70779426541d3e477ff2b1f77fb54
MrWolf0 <nassermessi33@gmail.com>
2024-01-09 00:32:28 +0200

    task 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     return_value = ""
     for letter in my_string:
-        if letter != 'c' or letter != 'C':
+        if letter != 'c' and letter != 'C':
             return_value += letter
     return return_value
