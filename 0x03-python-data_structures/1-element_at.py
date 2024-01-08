392b60def577a70b986f6fc9f8d6509f4fd7ad2a
MrWolf0 <nassermessi33@gmail.com>
2024-01-08 23:34:30 +0200

    task 3

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+def element_at(my_list, idx):
+    # Check if idx is negative
+    if idx < 0:
+        return None
+
+    # Check if idx is within the range of the list
+    if idx < len(my_list):
+        return my_list[idx]
+
+    # Return None if idx is out of range
+    return None
