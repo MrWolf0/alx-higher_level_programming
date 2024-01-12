e07be6e593c05a9a9dc385b3d6a8c7bff8d2849a
MrWolf0 <nassermessi33@gmail.com>
2024-01-13 01:09:12 +0200

    square matrix using map and lambda exprision

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    # map function used here to execute
+    # the lambda expression on each row in matrix
+    # lambda take argument which num and then do
+    # the instruction after : which num*num.
+    # map will execute the lambda expression each
+    # time find a number in the matrix
+    # finally we cast the output to list object
     return ([list(map(lambda num: num * num, row)) for row in matrix])
