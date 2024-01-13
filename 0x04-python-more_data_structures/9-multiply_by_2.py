5e0a8556b5f3e26d5bf24f7a2723d0d11e1f398a
MrWolf0 <nassermessi33@gmail.com>
2024-01-13 02:12:10 +0200

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@ def multiply_by_2(a_dictionary):
     # over the dic .
     # lambda use the item [0] which consist the key
     # item[1] which consist the value then *2
-    # finally return a dict 
+    # finally return a dict
     return dict(map(lambda item: (item[0], item[1] * 2), a_dictionary.items()))
