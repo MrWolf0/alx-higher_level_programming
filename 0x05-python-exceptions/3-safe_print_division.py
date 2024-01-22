39e6b43208b9523d0b49ad120df9e4aecb254477
MrWolf0 <nassermessi33@gmail.com>
2024-01-22 20:56:29 +0200

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,11 @@ def safe_print_division(a, b):
         result = a / b
     except (TypeError, ZeroDivisionError):
         print("not allowed division by zero!")
-        return None
+        result = None
+    except TypeError:
+        """ Handle invalid types for operands """
+        print("Only integers can be divided!")
+        result = None
     finally:
         print("Inside result: {}".format(result))
     return result
