95cd3b3047ec9fa7368260d69da305e365a3c6db
MrWolf0 <nassermessi33@gmail.com>
2024-01-24 01:27:50 +0200

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
     """ the first methode will call in case we create an
     instance from Square it like the constructor in java
     even i didn't create it the class will call it """
+
     def __init__(self, size):
         """ using __ mean create it as a private access modifier """
         self.__size = size
