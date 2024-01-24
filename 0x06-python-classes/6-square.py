38e4446bdd72974b420ddf54448a77184f54e409
MrWolf0 <nassermessi33@gmail.com>
2024-01-24 02:21:10 +0200

    6. Coordinates of a square fix style

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,13 +51,12 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-
-
     def area(self):
         return self.__size * self.__size
 
     def my_print(self):
-        """Print the square with the character '#' and considering the position."""
+        """Print the square with the character '#' and
+        considering the position."""
         if self.__size == 0:
             print()
         else:
